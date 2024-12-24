0a7aef7fdcc593b8eeb6fd8b6043ce3a8c340770
VibhanshuV <VibhanshuV00@gmail.com>
2024-12-24 21:51:58 +0530

    fixed devDependency issue for tailwind daisyui

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,6 +3,7 @@ import daisyUIThemes from "daisyui/src/theming/themes";
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	darkMode: 'media', // or 'class'
 	theme: {
 		extend: {},
 	},
